71f4764a1422e18551177c6efd45f44af2cd6bd7
unknown <vrnakano@gmail.com>
2020-12-01 16:32:05 -0300

    Implementação de rotas no blog pessoal

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'home', component: HomeComponent },
+{ path:'', redirectTo:'home', pathMatch:'full'},
+{ path: 'feed', component: FeedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
